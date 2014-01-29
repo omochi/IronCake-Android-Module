27f7f02d2bcff1923ed5f0ddd8ca961381103135
omochimetaru <omochi.metaru@gmail.com>
2014-01-30 00:21:51 +0900

    Timer不要

--- src/main/java/com/omochimetaru/ironcake/Activity.java
+++ src/main/java/com/omochimetaru/ironcake/Activity.java
@@ -4,15 +4,8 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Created by omochi on 2014/01/24.
@@ -46,7 +39,6 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
 
     private Handler mainThreadHandler;
     private SurfaceView surfaceView;
-    private Timer updateTimer;
 
     //ネイティブ化してオーバライドするように
     protected abstract long controllerConstruct();
@@ -63,8 +55,6 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
 
         setContentView(surfaceView);
 
-        updateTimer = new Timer();
-
         nativeOnCreate();
     }
 
@@ -73,7 +63,6 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
     @Override
     protected void onDestroy(){
         nativeOnDestroy();
-
         super.onDestroy();
     }
 
@@ -82,20 +71,14 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
     @Override
     protected void onResume(){
         super.onResume();
-
         nativeOnResume();
-
-        //onUpdateTimer();
     }
 
     private native void nativeOnResume();
 
     @Override
     protected void onPause(){
-        updateTimer.cancel();
-
         nativeOnPause();
-
         super.onPause();
     }
 
@@ -103,7 +86,6 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
 
     @Override
     public native void surfaceCreated(SurfaceHolder surfaceHolder);
-
     @Override
     public native void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height);
 
@@ -112,21 +94,4 @@ public abstract class Activity extends android.app.Activity implements SurfaceHo
 
     private native void update();
 
-    private void scheduleUpdateTimer(float delay){
-        updateTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                onUpdateTimer();
-            }
-        } , (int)(delay * 1000));
-    }
-    private void onUpdateTimer(){
-        //メインスレで同期更新する
-        mainThreadHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                Activity.this.update();
-            }
-        });
-    }
 }
